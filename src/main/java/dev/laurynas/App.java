1c1700338d327f5b55bd4c261c6b9bfc98e6008c
Laurynas <laurynas1narbutas@gmail.com>
2023-03-29 16:02:32 +0300

    Commit

--- src/main/java/dev/laurynas/App.java
+++ src/main/java/dev/laurynas/App.java
@@ -1,7 +1,82 @@
 package dev.laurynas;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Random rnd = new Random();
+
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = rnd.nextInt(-10, 11);
+        }
+
+        var numsList = numArrToStringList(nums);
+        System.out.println(numsList);
+
+        System.out.println("*".repeat(50));
+
+        System.out.println(Arrays.toString(nums));
+        numsArrInfo(nums);*/
+
+
+    }
+
+    public static Optional<TreeSet<String>> findPlants(String type) {
+        Plant[] plants = {
+                new Plant("Obuolys", "Vaisius"),
+                new Plant("Kriause", "Vaisius"),
+                new Plant("Morka", "Darzove"),
+                new Plant("Agurkas", "Darzove"),
+                new Plant("Bulve", "Darzove"),
+                new Plant("Vysnia", "Uoga")
+        };
+
+        TreeMap<String, TreeSet<String>> groupedPlants = Arrays.stream(plants)
+                .collect(Collectors.groupingBy(Plant::getType, TreeMap::new, Collectors.mapping(Plant::getName,Collectors.toCollection(TreeSet::new))));
+
+        TreeSet<String> result = groupedPlants.get(type);
+
+        return Optional.ofNullable(result);
+    }
+
+    public static List<String> numArrToStringList(int[] nums) {
+        return Arrays.stream(nums)
+                .filter(x -> x < 0)
+                .map(x -> x * x)
+                .sorted()
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    public static void numsArrInfo(int[] nums) {
+        int min = Arrays.stream(nums).min().orElse(0);
+        int max = Arrays.stream(nums).max().orElse(0);
+        long count = Arrays.stream(nums).count();
+        int sum = Arrays.stream(nums).sum();
+        long product = Arrays.stream(nums).reduce((a, b) -> a * b).orElse(0);
+        double average = Arrays.stream(nums).average().orElse(0);
+        System.out.printf("""
+                min = %d
+                max = %d
+                count = %d
+                sum = %d
+                product = %d
+                average = %f
+                """, min, max, count, sum, product, average);
+
+        int[][] nums2 = {
+                {1, 2, 3}, {4, 5, 6}, {7, 8, 9}
+        };
+
+        System.out.println(sumOfArrArr(nums2));
+    }
+
+    public static int sumOfArrArr(int[][] nums) {
+        return Arrays.stream(nums)
+                .flatMapToInt(Arrays::stream)
+                .sum();
     }
 }
---
+++ src/main/java/dev/laurynas/Plant.java
@@ -0,0 +1,36 @@
+package dev.laurynas;
+
+public class Plant {
+    String name;
+    String type;
+
+    public Plant(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Plant{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
