61940a747811d4adb94e28d52e424cc5ced349ca
Laurynas <laurynas1narbutas@gmail.com>
2023-03-31 16:01:44 +0300

    Commit

--- src/main/java/dev/laurynas/App.java
+++ src/main/java/dev/laurynas/App.java
@@ -1,7 +1,14 @@
 package dev.laurynas;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Pattern a = Pattern.compile("\\w");
+        Matcher b = a.matcher("aaaa sample text");
+        b.reset()
+
     }
 }
